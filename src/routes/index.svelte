db66d3cd67dc48b9d222651531f6755a30910efc
Mark Naberukhin <mna@startext.ru>
2021-06-03 17:52:27 +0700

    :tada: another init

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+  <title>Admin FUQs</title>
+</svelte:head>
+
+<h1>Admin FUQs</h1>
+<ul>
+  <li>FUQ 1</li>
+  <li>FUQ 2</li>
+  <li>FUQ 3</li>
+  <li>FUQ 4</li>
+  <li>FUQ 5</li>
+</ul>
