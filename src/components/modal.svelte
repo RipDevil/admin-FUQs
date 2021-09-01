8e472f07c00249131205369dc21e5cf3fc90adca
RipDevil <tiberi64@yandex.ru>
2021-09-01 14:42:07 +0700

    :sparkles: implementing modal window

---
+++ src/components/modal.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export const title = 'Something';
+    export let visible: boolean;
+    // TODO: smooth animation
+</script>
+
+{#if visible}
+    <div class="absolute z-50 w-full h-full bg-gray-200 flex flex-col items-center justify-center overflow-hidden">
+        <div class="w-1/3 rounded-md bg-gray-50 p-2 shadow-md">
+            <div class="text-center mb-2 text-lg">{title}</div>
+            <div class="flex flex-col space-y-1">
+                <slot />
+            </div>
+        </div>
+    </div>
+{/if}
--- src/pages/users-page.svelte
+++ src/pages/users-page.svelte
@@ -3,6 +3,7 @@
     import { getUsers, users } from '../models/users';
 
     import Layout from '../layout/index.svelte';
+    import Modal from '../components/modal.svelte';
 
     onMount(() => {
         getUsers();
@@ -13,8 +14,15 @@
     <title>Users Page</title>
 </svelte:head>
 
+<!-- <Modal visible={true}>
+    <div>Test</div>
+</Modal> -->
+
 <Layout title="Users Page">
     <div class="mt-5 space-y-2">
+        <div class="p-2 rounded-md bg-green-100 hover:bg-green-200 text-xl cursor-pointer flex flex-row justify-center">
+            <div>Create new user</div>
+        </div>
         {#each $users as user}
             <div
                 class="p-2 rounded-md bg-gray-100 hover:bg-gray-200 text-xl cursor-pointer flex flex-row justify-between"
